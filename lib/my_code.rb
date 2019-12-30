ba4e5f89ddf4b836ac78d7f975a29c3ad32e26d4
David Holmes <phoenix1988@gmail.com>
2019-12-30 21:20:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,17 +9,16 @@ def map(array)
 end
 
 def reduce(array, sv=nil)
-  sum = 0
   if sv
-    num1 = sv
+    sum = sv
     i = 0
   else
-    num1 = array[0]
+    sum = array[0]
     i = 1
   end
   while i < array.length do
-    num1 += yield(num1, array[i])
+    sum += yield(sum, array[i])
     i += 1
   end
-  num1 
+  sum
 end
